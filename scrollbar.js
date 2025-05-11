74c9ab689ce0189fea8ebdae631f2f1665614116
fullStack58 <pipelontdk@hotmail.com>
2025-05-10 21:31:21 -0500

    Cambios mayores

--- scrollbar.js
+++ scrollbar.js
@@ -1,28 +1,25 @@
-// document.querySelector: Seleccionará un elemento del documento que tenga una clase o etiqueta en específico.
 const navbar = document.querySelector(".navbar");
+
 let navBarCollapse = false;
-let scroll;
+let scroll = 0;
+var threshold = 500; // Almacena el umbral (en píxeles) donde queremos activar el navbar fijo según el valor que nos de la variable anterior
 
 /*Evento que detecta cuando la página es scrolleada */
 window.addEventListener("scroll", () => {
     /* Almacena el número de pixeles por el que se scrollea la página */
     scroll = window.pageYOffset || window.scrollY;
 
-    // Almacena el umbral (en píxeles) donde queremos activar el navbar fijo según el valor que nos de la variable anterior
-    var threshold = 500;
-
-    /*Llamamos la función para fijar el navbar*/
     navbarFixed(scroll, threshold);
 });
 
 function navbarFixed(scroll, threshold) {
     // Si el scroll ha alcanzado el umbral, se añade la clase 'fixed' de (styles.css) al elemento (.navbar) que lo almacena la constante (const navbar).
-    if (scroll > threshold) {
+    if (scroll > threshold || navBarCollapse) {
         navbar.style.cssText =
-            "position: fixed; z-index: 101; padding:20px; background: rgba(0, 0, 0, 0.79); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(7.5px); -webkit-backdrop-filter: blur(7.5px);";
-    } else if (scroll < threshold && !navBarCollapse) {
+            "transition:background 0.2s ease-in-out; position:fixed; z-index: 101; padding:20px; background: rgba(0, 0, 0, 0.79); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(7.5px); -webkit-backdrop-filter: blur(7.5px);";
+    } else if (scroll < threshold) {
         navbar.style.cssText =
-            "position: absolute; top: 0; left: 0; width: 100%; padding:20px; z-index: 100; background-color: transparent;";
+            "transition:background 0.2s ease-in-out; position: absolute; top: 0; left: 0; width: 100%; padding:20px; z-index: 100; background-color: transparent;";
     }
 }
 
@@ -32,15 +29,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     navbarCollapse.addEventListener("shown.bs.collapse", () => {
         navBarCollapse = true;
-        navbar.style.cssText =
-            "position: fixed; z-index: 101; padding:20px; background: rgba(0, 0, 0, 0.79); box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(7.5px); -webkit-backdrop-filter: blur(7.5px);";
+        navbarFixed(scroll, threshold);
     });
 
     navbarCollapse.addEventListener("hidden.bs.collapse", () => {
         navBarCollapse = false;
-        if (scroll < threshold) {
-            navbar.style.cssText =
-                "position: absolute; top: 0; padding:20px; left: 0; width: 100%; padding: 1rem 1rem; z-index: 100; background-color: transparent;";
-        }
+        navbarFixed(scroll, threshold);
     });
 });
